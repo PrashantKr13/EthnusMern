53cfe54fd1dd033a84e7bf7dcf746745d50f547f
PrashantKr13 <98947818+PrashantKr13@users.noreply.github.com>
2023-11-27 13:02:46 +0530

    Update Login.jsx

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        axios.post('http://localhost:3001/login', { email, password }).then(result => {
+        axios.post('http://localhost:3001/', { email, password }).then(result => {
             console.log(result)
             if (result.data === "Success") {
                 setisLogged(true)
